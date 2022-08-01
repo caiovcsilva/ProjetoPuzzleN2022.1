c5af4aa991d9f068065c0068d14909eec60064ed
Sérgio Henrique <72146555+TheSergioHenrique@users.noreply.github.com>
2022-08-01 13:43:17 -0300

    Pasta deletada para melhor organização.

--- ProjetoPuzzleN2022.1/src/PuzzleN/Jogador.java
+++
@@ -1,9 +0,0 @@
-package PuzzleN;
-
-public class Jogador {
-	private String nome;
-	
-	public Jogador(String nome){
-        this.nome = nome;
-    }
-}
--- ProjetoPuzzleN2022.1/src/Teste/Teste.java
+++
@@ -1,7 +0,0 @@
-package Teste;
-
-public class Teste {
-	public static void main (String[]args) {
-		
-	}
-}
