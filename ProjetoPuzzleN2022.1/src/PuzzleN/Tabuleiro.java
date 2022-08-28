d3c943bc82de3f5f99097cf63101a7afc4ba992a
Caio Victor <98838182+caiovcsilva@users.noreply.github.com>
2022-08-28 20:19:58 -0300

    Início do tabuleiro 🌟
    Feito: - Embaralhamento de peças
    - Printar a lista de peças no console em ordem por tabulação
    - Iniciar o tabuleiro
    
    falta: (botão de exibição de posições corretas)
    (destacar quando a peça está na posição correta)

--- ProjetoPuzzleN2022.1/src/PuzzleN/Tabuleiro.java
+++ ProjetoPuzzleN2022.1/src/PuzzleN/Tabuleiro.java
@@ -1,49 +1,63 @@
 package PuzzleN;
-import java.util.Random;
 
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
 
-public class Tabuleiro extends Menu{
+public class Tabuleiro{
+	public int tamanho;
+	private String []pecas;
+	private String[][] totalPecas;
 	
-	//tamanho de nosso tabuleiro. Nesse caso, no facil, um 3x3.
-	private int tamanhoTabuleiro = 3;
-	//para saber quais peças estão em qual lugar.
-	private Peca [][] quadrados;
-	//quando o jogo acabar, é true.
-	private boolean fimDeJogo;
 	
-	//Roda o metodo SetTamanhoTabuleiro e define o escopo do tabuleiro.
-	public Tabuleiro(int dificuldade) {
-		setTamanhoTabuleiro(dificuldade);
-		this.quadrados = new Peca[this.tamanhoTabuleiro][this.tamanhoTabuleiro];
+	
+	private Peca[][] arrayCasa = new Peca[tamanho][tamanho]; // neste exemplo são o tamanho inserido pelo usuário
+
+	public Tabuleiro(int tamanho) {
+		setTamanho(tamanho);
+	}
+	
+	public int setTamanho(int tamanho) {
+		return this.tamanho = tamanho;
 	}
 	
 	
-	//Método que cria o tabuleiro, de acordo com a dificuldade que vai ser escolhida pelo usuario.
-	public void setTamanhoTabuleiro(int dificuldade) {
-		if ( dificuldade == 2) { 
-			++this.tamanhoTabuleiro;
-		}else if (dificuldade == 3) {
-			this.tamanhoTabuleiro += 2;
+	public Peca getCasa(int linha, int coluna){
+		return arrayCasa[linha][coluna];
+	}
+
+	public void setCasa(int linha, int coluna, Peca peca){
+		arrayCasa[linha][coluna] = peca;
+
+	}
+	
+	public void alocarPosicoes() { 
+		String[] tempArray = new String[this.tamanho*this.tamanho]; 
+		for(int i = 0; i < this.tamanho*this.tamanho; i++) {
+			tempArray[i] = "[" + String.valueOf(i + 1) + "]";
 		}
+		this.pecas = tempArray; 
+		pecas[pecas.length - 1] = "[ ]";
 	}
-	
 
-	//O iniciar, irá espalhar as peças, aleatoriamente, no tabuleiro; Se baseando na dificuldade escolhida para setar o seu
-	public void iniciar() {
-		int tabuleiroNxN = this.tamanhoTabuleiro*this.tamanhoTabuleiro;
-		Random valorRandomPecas = new Random();
-		int X_random;
-		int Y_random;
+	public void iniciarTabuleiro() {
 		
-		for (int n=1;tabuleiroNxN>n;n++) {
-			do {
-				X_random = valorRandomPecas.nextInt(this.tamanhoTabuleiro-1);
-			 	Y_random = valorRandomPecas.nextInt(this.tamanhoTabuleiro-1);
-			} while(quadrados[X_random][Y_random].hasNumeroPeca);
-			quadrados[X_random][Y_random].setnumeroDaPeca(n);
-		}
+		
 	}
 	
-
+	public void misturarNumeros() { 
+        List<String> list =Arrays.asList(this.pecas); 
+        Collections.shuffle(list);
+        list.toArray(this.pecas);
+	}
+	
+	public void exibeTabuleiro() { 
+        for(int i = 0; i < this.tamanho; i++) {
+            for(int j = 0; j < this.tamanho; j++) {
+                System.out.print(this.totalPecas[i][j] + "\t"); 
+            }
+            System.out.println();
+        }
+	}
 	
 }
