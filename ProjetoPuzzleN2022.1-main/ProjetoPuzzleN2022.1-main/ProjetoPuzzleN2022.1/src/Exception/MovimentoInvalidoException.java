1288b6b12431cda0fa95e0c99d2118db9619ba67
Caio Victor <98838182+caiovcsilva@users.noreply.github.com>
2022-09-11 21:42:13 -0300

    Adição da fonte e dos botões da interface ⚙️
    Ainda falta a programação dos eventos!

---
+++ ProjetoPuzzleN2022.1-main/ProjetoPuzzleN2022.1-main/ProjetoPuzzleN2022.1/src/Exception/MovimentoInvalidoException.java
@@ -0,0 +1,10 @@
+package Exception;
+
+public class MovimentoInvalidoException extends Exception {
+
+	@Override
+	public String toString() {
+		return "Movimento inválido da peça";
+	}
+	
+}
---
+++ ProjetoPuzzleN2022.1-main/ProjetoPuzzleN2022.1-main/ProjetoPuzzleN2022.1/src/PuzzleN/Menu.java
@@ -0,0 +1,49 @@
+package PuzzleN;
+import java.util.*;
+
+public class Menu {
+	private int dificuldade = 1;
+	
+	public int getDificuldade() {
+		return dificuldade;
+	}
+	
+	public void iniciarMenu() {
+		Scanner sc= new Scanner(System.in);
+		System.out.println("JOGAR -1");
+		System.out.println("OPCOES -2");
+		int input = sc.nextInt();
+		
+		switch (input) {
+		case 1:jogar(); break;
+		case 2: opcoes(); break;
+		default: sair();
+		}
+	}
+	
+	
+	
+	public void jogar() {
+		Tabuleiro jogoAtivo = new Tabuleiro(dificuldade);
+		jogoAtivo.exibeTabuleiro();
+	}
+	
+	public void acessarRanking() {
+		
+	}
+	
+	public void opcoes() {
+		System.out.println("Altere a dificuldade do jogo! Escolha entre 1 a 3.");
+		Scanner sc= new Scanner(System.in);
+		this.dificuldade = sc.nextInt();
+		
+	}
+	
+	public void acessarCreditos() {
+		
+	}
+	
+	public void sair() {
+		
+	}
+}
